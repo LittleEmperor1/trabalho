6c85afe36b968f4638dfb2839e2cdbbf871a13fb
Eric <eric0580@hotmail.com>
2025-05-13 21:47:33 -0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,1 +1,5 @@
+// Função para mostrar a mensagem ao clicar no botão
+document.getElementById("mostrarMensagem").addEventListener("click", function() {
+    document.getElementById("mensagem").textContent = "Seja bem-vindo! Aproveite o seu tempo aprendendo!";
+});
 
